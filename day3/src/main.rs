8407027500863f502edfcd97c7eae1097516fedd
Dan <daniel.lauri22@gmail.com>
2023-12-03 12:56:27 +0100

    day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
     let input = include_str!("input1.txt");
 
-    let result_1 = solve_1(input);
     println!("---RESULT_1---");
+    let result_1 = solve_1(input);
     println!("{}", result_1);
+    println!("---RESULT_2---");
+    let result_2 = solve_2(input);
+    println!("{}", result_2);
 }
 
 fn solve_1(input: &str) -> u32 {
@@ -23,7 +26,6 @@ fn solve_1(input: &str) -> u32 {
                             continue;
                         } else {
                             let res = check_for_num(&mat, i, j);
-                            println!("RESULT {:?} {}", res, j);
                             sum += res.0;
                             j += res.1 - j;
                             if j > col + 1 {
@@ -32,6 +34,48 @@ fn solve_1(input: &str) -> u32 {
                         }
                     }
                 }
+            }
+        }
+    }
+    sum
+}
+fn solve_2(input: &str) -> u32 {
+    let mat: Vec<_> = input.split('\n').collect();
+    let mut sum = 0;
+
+    for (row, str) in mat.iter().enumerate() {
+        for (col, char) in str.chars().enumerate() {
+            if char == '*' {
+                let mut nums = [0, 0];
+                let mut gear_numbers = 0;
+
+                for i in row - 1..=row + 1 {
+                    let mut j = if col == 0 { 0 } else { col - 1 };
+
+                    loop {
+                        if i == row && j == col {
+                            j += 1;
+                            continue;
+                        } else {
+                            let res = check_for_num(&mat, i, j);
+                            if res.0 > 0 {
+                                nums[gear_numbers] = res.0;
+                                gear_numbers += 1;
+                            }
+                            // It means there were more than 2 numbers adjacent to a gear
+                            if gear_numbers > 2 {
+                                nums[1] = 0;
+                            }
+
+                            j += res.1 - j;
+                            if j > col + 1 {
+                                break;
+                            };
+                        }
+                    }
+                }
+
+                sum += nums[0] * nums[1];
             }
         }
     }
@@ -52,7 +96,6 @@ fn check_for_num(mat: &Vec<&str>, row: usize, col: usize) -> (u32, usize) {
             while right < chars.len() && chars[right].is_ascii_digit() {
                 right += 1;
             }
-            println!("FOUND SLICE {}", &str[left..right]);
 
             res = str[left..right].parse().unwrap();
         }
